9ee86a06793d17dd88d338611df2fd25fa5f8a6e
Davie <daov31501@gmail.com>
2026-06-22 14:50:21 +0700

    Update Controller.java

--- scene_output/Controller.java
+++ scene_output/Controller.java
@@ -1,3 +1,10 @@
+package scene_output;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 public class Controller {
 
     @FXML
