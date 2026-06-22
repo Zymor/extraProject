fa8a677d4dab9e713f2d84769e521410c029ec16
Starling <nakboyloy50@gmail.com>
2026-06-22 14:08:19 +0700

    up

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Double num1 = 10.5;
+
+        Number num2 = (Number) num1;
+        System.out.println(num2);
+    }
+}
--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -2,8 +2,12 @@ package scene_main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class Controller {
 
@@ -20,7 +24,20 @@ public class Controller {
 
     @FXML
     void GO_TO_OUPUT_BUTTON(ActionEvent event) {
-        Stage = Label_data.getScene().getWindow();
+       var stage = (Stage) Label_data.getScene().getWindow(); 
+
+       var view_output = getClass().getResource("/scene_output/scene_output.fxml");
+       var controller_output = new scene_output.Controller();
+
+       var loader = new FXMLLoader();
+       loader.setLocation(view_output);
+       loader.setController(controller_output);
+
+        // 
+        var scence = new Scene(loader.load());
+        //
+        stage.setScene(scence); 
+        stage.show();
 
     }
 
